f0171bac6a562ac52d33b6b31c72adbd6d4bde57
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-12-03 22:41:04 +0300

    'update'

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -5,7 +5,7 @@ function Error() {
     <div id="error-page" className="w-screen mt-8 flex flex-col items-center">
       <h1 className="text-red-700 text-4xl mb-2">Oops!</h1>
       <p className="text-xl  font-semibold mt-2">
-        Sorry, an unexpected error has occurred.
+        Sorry, an unexpected error has occurred !.
       </p>
       <Link to="/" className="mt-4 underline text-2xl font-extrabold">
         Back to Home
