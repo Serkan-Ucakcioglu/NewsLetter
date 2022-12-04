b5a761345b2361cb39849343a6d6b55dde77834e
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-12-04 12:09:01 +0300

    css fix

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,7 +5,7 @@ import Navbar from "./Navbar";
 function Header() {
   return (
     <header className="w-full bg-gray-100 shadow-sm">
-      <div className="container w-11/12 h-24 flex px-10 justify-center items-center p-5 m-auto bg-gray-100">
+      <div className="container w-11/12 h-24 flex px-10 justify-center items-center p-5 m-auto">
         <div className="logo mr-auto">
           <Logo />
         </div>
