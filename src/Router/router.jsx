d1b3c73b1d6b3d1e3afef16619c5a68efc6ecf1c
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-12-16 17:06:47 +0300

    error element add router

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -11,7 +11,7 @@ import Tabs from "../Features/Topics/Tabs/Tabs";
 export const router = createHashRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      <Route index element={<Tabs />} />
+      <Route index element={<Tabs />} errorElement={<Error />} />
       <Route path="detail/:id" element={<DetailNews />} />
       <Route path="*" element={<Error />} />
     </Route>
