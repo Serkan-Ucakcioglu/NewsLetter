40aa8aa95fb71a25663221be168184a1fe0d9206
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-12-16 15:08:31 +0300

    update css

--- src/Features/Topics/Finance/FinanceList.jsx
+++ src/Features/Topics/Finance/FinanceList.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function FinanceList({ topic }) {
   const brands = topic?.secondaryBrands?.map((brand) => {
     return (
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+      <span className="inline-block self-end bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
         #{brand}
       </span>
     );
@@ -30,14 +30,14 @@ function FinanceList({ topic }) {
       </span>
     </>
   ) : (
-    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+    <span className="inline-block self-end bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
       #{topic?.primarySite}
     </span>
   );
   return (
     <div className="flex flex-col  items-center mb-2 mt-8">
       <article
-        className={`w-[350px] bg-white border   border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 ${
+        className={`w-[350px]  bg-white border   border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 ${
           topic.id ? "h-[500px]" : "h-[420px]"
         }`}
       >
@@ -52,35 +52,41 @@ function FinanceList({ topic }) {
           alt="image"
         />
 
-        <div className="p-5">
-          <h5 className="mb-2 text-m font-bold text-gray-900 dark:text-white">
-            {topic?.byline}
-          </h5>
+        <div className="flex flex-1  flex-col p-5">
+          <div className="flex flex-col justify-center h-[110px]">
+            <h5 className="mb-2 text-m font-bold text-gray-900 dark:text-white">
+              {topic?.byline}
+            </h5>
 
-          <p className="mb-3 text-left text-sm font-normal text-gray-700 dark:text-gray-400">
-            {topic?.title}
-          </p>
-          <Link
-            to={`detail/${topic?.internalID}`}
-            className="inline-flex mt-auto mb-2 items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Read more
-            <svg
-              aria-hidden="true"
-              className="w-4 h-4 ml-2 -mr-1"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
+            <p className="mb-3 text-left text-sm font-normal text-gray-700 dark:text-gray-400">
+              {topic?.title}
+            </p>
+          </div>
+          <div className="flex flex-col justify-end">
+            <Link
+              to={`detail/${topic?.internalID}`}
+              className="inline-flex mt-auto mb-2 items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              <path
-                fillRule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </Link>
-          <div className="flex justify-start items-center mt-2">{primary}</div>
-          <div className="flex flex-wrap mt-3">{brands}</div>
+              Read more
+              <svg
+                aria-hidden="true"
+                className="w-4 h-4 ml-2 -mr-1"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </Link>
+            <div className="flex justify-start items-center h-full ">
+              {primary}
+            </div>
+            <div className="flex flex-wrap mt-3 h-full items-end">{brands}</div>
+          </div>
         </div>
       </article>
     </div>
