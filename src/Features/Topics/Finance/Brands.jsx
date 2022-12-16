ad3c27fcccaa7eff3f3c1908db5111924975670b
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-12-17 00:07:06 +0300

    primary svg component

--- src/Features/Topics/Finance/Brands.jsx
+++ src/Features/Topics/Finance/Brands.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Primary from "../../../Svg/Primary";
 
 function Brands({ topic }) {
   const brands = topic?.secondaryBrands?.map((brand) => {
@@ -11,19 +12,7 @@ function Brands({ topic }) {
 
   const primary = topic?.id ? (
     <>
-      <svg
-        className="opacity-75 mr-2"
-        version="1.1"
-        id="Capa_1"
-        x="0px"
-        y="0px"
-        width="12"
-        height="12"
-        viewBox="0 0 97.16 97.16"
-      >
-        <path d="M48.58,0C21.793,0,0,21.793,0,48.58s21.793,48.58,48.58,48.58s48.58-21.793,48.58-48.58S75.367,0,48.58,0z M48.58,86.823    c-21.087,0-38.244-17.155-38.244-38.243S27.493,10.337,48.58,10.337S86.824,27.492,86.824,48.58S69.667,86.823,48.58,86.823z" />
-        <path d="M73.898,47.08H52.066V20.83c0-2.209-1.791-4-4-4c-2.209,0-4,1.791-4,4v30.25c0,2.209,1.791,4,4,4h25.832    c2.209,0,4-1.791,4-4S76.107,47.08,73.898,47.08z" />
-      </svg>
+      <Primary />
       <span className="font-bold text-gray-500">
         {topic?.id?.substring(0, 10)}
       </span>
