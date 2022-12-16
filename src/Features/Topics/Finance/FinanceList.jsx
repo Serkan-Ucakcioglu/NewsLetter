bc96f13e303ea4d50daea82f269ceccf3fa7b59b
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-12-16 16:49:07 +0300

    read more buttons update

--- src/Features/Topics/Finance/FinanceList.jsx
+++ src/Features/Topics/Finance/FinanceList.jsx
@@ -38,7 +38,7 @@ function FinanceList({ topic }) {
           <div className="flex flex-col justify-end">
             <Link
               to={`detail/${topic?.internalID}`}
-              className="inline-flex mt-auto mb-2 items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="inline-flex w-[120px] mt-auto mb-2 items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Read more
               <svg
