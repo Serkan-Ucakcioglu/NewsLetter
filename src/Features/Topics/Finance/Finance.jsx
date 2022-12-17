792facecfc549c3bd998b20d5b1d4391c5f0757d
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-12-17 19:02:07 +0300

    dark mode class add

--- src/Features/Topics/Finance/Finance.jsx
+++ src/Features/Topics/Finance/Finance.jsx
@@ -15,19 +15,19 @@ function Finance() {
 
   return (
     <>
-      <div className="w-full flex justify-center pt-4 dark:bg-gray-800">
+      <div className="w-full flex flex-col items-center pt-4 dark:bg-gray-800">
         <div className="flex flex-wrap justify-center max-w-6xl mx-auto gap-3">
           {currentSearchData &&
             currentSearchData?.map((topic) => (
               <TopicList topic={topic} key={topic?.internalID} />
             ))}
         </div>
+        <Pagination
+          currentPage={currentPage}
+          totalCount={data && data?.length}
+          onPageChange={(page) => setCurrentPage(page)}
+        />
       </div>
-      <Pagination
-        currentPage={currentPage}
-        totalCount={data && data?.length}
-        onPageChange={(page) => setCurrentPage(page)}
-      />
     </>
   );
 }
