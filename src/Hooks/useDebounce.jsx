4a43475d181dcdf52691d562762e66e30fd21926
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-12-19 00:45:22 +0300

    update

--- src/Hooks/useDebounce.jsx
+++ src/Hooks/useDebounce.jsx
@@ -6,7 +6,7 @@ function useDebounce(value, delay) {
   const dispatch = useDispatch();
   const debounce = useSelector(selectedDebounce);
   useEffect(() => {
-    let timer = setTimeout(() => dispatch(addDebounce(value)), delay || 400);
+    let timer = setTimeout(() => dispatch(addDebounce(value)), delay || 300);
 
     return () => {
       clearTimeout(timer);
