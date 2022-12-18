d1e4422403dab5d04de54922056cababbe755d32
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-12-18 12:56:43 +0300

    indent

--- src/Hooks/useDebounce.jsx
+++ src/Hooks/useDebounce.jsx
@@ -5,7 +5,6 @@ import { addDebounce, selectedDebounce } from "../Features/searchSlices";
 function useDebounce(value, delay) {
   const dispatch = useDispatch();
   const debounce = useSelector(selectedDebounce);
-
   useEffect(() => {
     let timer = setTimeout(() => dispatch(addDebounce(value)), delay || 500);
 
