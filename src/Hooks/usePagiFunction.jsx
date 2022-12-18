a2187e850e2187605efe36e4eada939fa7407a74
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-12-18 12:10:51 +0300

    update

--- src/Hooks/usePagiFunction.jsx
+++ src/Hooks/usePagiFunction.jsx
@@ -3,7 +3,6 @@ import { useMemo, useState } from "react";
 function usePagiFunction(datas) {
   let PageSize = 6;
   const [currentPage, setCurrentPage] = useState(1);
-
   const currentSearchData = useMemo(() => {
     const firstPageIndex = (currentPage - 1) * PageSize;
     const lastPageIndex = firstPageIndex + PageSize;
