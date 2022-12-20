53f734db5b95f8e0d0e35f009c4a431c86909f83
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-12-20 12:19:36 +0300

    indent

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,7 +5,7 @@ import Search from "../../Features/Search/Search";
 function Header() {
   return (
     <header className="w-full  bg-white p-2 shadow-sm dark:bg-gray-800">
-      <div className="container w-11/12 h-24 flex justify-around px-4 sm:px-10 sm:justify-evenly  items-center p-5 m-auto">
+      <div className="container w-11/12 h-24 flex justify-around px-4 sm:px-10 sm:justify-evenly items-center p-5 m-auto">
         <Logo />
         <Search />
         <ThemeMode />
