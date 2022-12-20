a2ab73549a99772071492178b8394d23a25bc8d2
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-12-20 12:20:38 +0300

    indent

--- src/Hooks/useDebounce.jsx
+++ src/Hooks/useDebounce.jsx
@@ -5,6 +5,7 @@ import { addDebounce, selectedDebounce } from "../Features/searchSlices";
 function useDebounce(value, delay) {
   const dispatch = useDispatch();
   const debounce = useSelector(selectedDebounce);
+
   useEffect(() => {
     let timer = setTimeout(() => dispatch(addDebounce(value)), delay || 300);
 
--- src/Hooks/usePagiFunction.jsx
+++ src/Hooks/usePagiFunction.jsx
@@ -3,6 +3,7 @@ import { useMemo, useState } from "react";
 function usePagiFunction(datas) {
   let PageSize = 6;
   const [currentPage, setCurrentPage] = useState(1);
+
   const currentSearchData = useMemo(() => {
     const firstPageIndex = (currentPage - 1) * PageSize;
     const lastPageIndex = firstPageIndex + PageSize;
