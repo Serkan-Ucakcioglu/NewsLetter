12c35d39f8a0b811287cc6086177e7f45436b9bb
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-12-30 09:44:53 +0300

    update

--- src/Features/Search/SearchExperct.jsx
+++ src/Features/Search/SearchExperct.jsx
@@ -12,7 +12,7 @@ function SearchExperct({ obj }) {
         to={`/detail/${obj?.id}`}
         className="inline-flex  flex-row w-[125px] mt-auto mb-2 items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
-        Go To Detail
+        Go To Details
         <ReadMore />
       </Link>
     </div>
