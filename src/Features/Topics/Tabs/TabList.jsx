8cdd1a2a619959ec81f7ce4584bc6488624aea66
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-02-02 12:20:28 +0300

    updated

--- src/Features/Topics/Tabs/TabList.jsx
+++ src/Features/Topics/Tabs/TabList.jsx
@@ -4,9 +4,8 @@ function TabList({ obj, setActive, active }) {
   return (
     <li className="mr-2">
       <a
-        href="#"
         onClick={() => setActive(obj?.name)}
-        className={`inline-block p-4 rounded-t-lg border-b-2  border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 ${
+        className={`inline-block cursor-pointer p-4 rounded-t-lg border-b-2  border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 ${
           active === obj.name ? "text-blue-600 border-b-2 border-blue-600" : ""
         }`}
       >
