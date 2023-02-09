df87b3ac8bc0a8ad700d67e85bc3ef872bfa914a
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-02-09 16:49:34 +0300

    clear component

--- src/Features/Topics/News/News.jsx
+++ src/Features/Topics/News/News.jsx
@@ -1,31 +1,13 @@
 import Loader from "../../../Svg/Loader";
-import FinanceList from "../Finance/FinanceList";
 import { useGetNewsQuery } from "../topicsSlice";
-import Pagination from "../../../Components/Pagination/Pagination";
-import usePagiFunction from "../../../Hooks/usePagiFunction";
+import useContent from "../../../Hooks/useContent";
 
 function News() {
   const { data, isFetching } = useGetNewsQuery();
-  const { currentSearchData, currentPage, setCurrentPage } = usePagiFunction(
-    data && data
-  );
+  const content = useContent(data && data);
 
   if (isFetching) return <Loader />;
-  return (
-    <div className="flex flex-col dark:bg-gray-800">
-      <div className="flex flex-wrap justify-center max-w-6xl mx-auto gap-3">
-        {currentSearchData &&
-          currentSearchData?.map((topic) => {
-            return <FinanceList topic={topic} key={topic?.internalID} />;
-          })}
-      </div>
-      <Pagination
-        currentPage={currentPage}
-        totalCount={data && data?.length}
-        onPageChange={(page) => setCurrentPage(page)}
-      />
-    </div>
-  );
+  return <>{content}</>;
 }
 
 export default News;
