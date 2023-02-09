6badfeeacb7ccb170b1cc8b437e8cd36ff807e9a
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-02-09 16:40:33 +0300

    logo updated

--- src/Components/Header/Logo.jsx
+++ src/Components/Header/Logo.jsx
@@ -5,7 +5,7 @@ function Logo() {
   return (
     <Link to="/" className="mr-10">
       <img
-        src="https://cdn-icons-png.flaticon.com/512/21/21601.png"
+        src="https://t3.ftcdn.net/jpg/00/88/43/58/360_F_88435823_c3MiOAvV8gFwtQzTGlsLt6I6mFvQuQmN.jpg"
         alt="logo"
         className="rounded  h-10 sm:w-10 sm:h-10 dark:bg-white p-1"
         loading="lazy"
