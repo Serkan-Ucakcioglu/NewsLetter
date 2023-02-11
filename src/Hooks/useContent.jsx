4430ec29a73aea104ed12e3bd0350000af62a36f
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-02-11 10:14:43 +0300

    comment line add

--- src/Hooks/useContent.jsx
+++ src/Hooks/useContent.jsx
@@ -7,6 +7,9 @@ function useContent(data) {
   const { currentSearchData, currentPage, setCurrentPage } = usePagiFunction(
     data && data
   );
+
+  //Instead of mapping the component one by one, returning the component,
+  // by dipping it once and preventing the duplication of the code.
   const content = (
     <div className="flex flex-col dark:bg-gray-800">
       <div className="flex flex-wrap justify-center max-w-6xl mx-auto gap-3">
