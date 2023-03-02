5b74aae5ed07953175f777b2a11f126fb9804fc7
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-03-02 12:25:55 +0300

    logo updated

--- src/Components/Header/Logo.jsx
+++ src/Components/Header/Logo.jsx
@@ -5,7 +5,7 @@ function Logo() {
   return (
     <Link to="/" className="mr-10">
       <img
-        src="https://t3.ftcdn.net/jpg/00/88/43/58/360_F_88435823_c3MiOAvV8gFwtQzTGlsLt6I6mFvQuQmN.jpg"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8mB1pyuU6HAxkxPSXsdiuSzknXuNPtNIG4Q&usqp=CAU"
         alt="logo"
         className="rounded  h-10 sm:w-10 sm:h-10 dark:bg-white p-1"
         loading="lazy"
