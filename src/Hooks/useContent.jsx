7d0320294792dfe1ffa450358f338ff896867c8f
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-03-15 11:25:05 +0300

    update

--- src/Hooks/useContent.jsx
+++ src/Hooks/useContent.jsx
@@ -11,7 +11,7 @@ function useContent(data) {
   //Instead of mapping the component one by one, returning the component,
   // by dipping it once and preventing the duplication of the code.
   const content = (
-    <div className="flex flex-col dark:bg-gray-800">
+    <div className="flex flex-col dark:bg-gray-800 dark:h-screen">
       <div className="flex flex-wrap justify-center max-w-6xl mx-auto gap-3">
         {currentSearchData &&
           currentSearchData?.map((topic) => {
